15f75ddffb0d36a750e8ad1ba639ed9de70434b6
Aidan Weatherill <aidan@fo.am>
2017-04-27 16:03:28 +0100

    move connection strings to app.config

--- code/Application/psDataImporter.Console/Program.cs
+++ code/Application/psDataImporter.Console/Program.cs
@@ -1,28 +1,29 @@
-﻿using NLog;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Data.OleDb;
 using Dapper;
-using System.Collections.Generic;
-using System.Data;
+using NLog;
 using Npgsql;
 
 namespace psDataImporter.Console
 {
-    class Program
+    internal class Program
     {
-        private static Logger logger = LogManager.GetCurrentClassLogger();
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             IEnumerable<NewLifeHistory> data = new List<NewLifeHistory>();
             // attach to access
             try
             {
-                 data = GetLifeHistoryDataFromAccess();
+                data = GetLifeHistoryDataFromAccess();
             }
             catch (Exception ex)
             {
-                logger.Error(ex, "Access error");
+                logger.Error(ex, "Access error"+ ex.Message);
             }
 
             // attach to postgres
@@ -32,10 +33,9 @@ namespace psDataImporter.Console
             }
             catch (Exception ex)
             {
-
                 logger.Error(ex, "postgres error" + ex.Message);
             }
-           
+
 
             // import all packs
             // import all litters
@@ -49,51 +49,48 @@ namespace psDataImporter.Console
 
         private static void PushDataToPostgres(IEnumerable<NewLifeHistory> data)
         {
-            using (IDbConnection conn = new NpgsqlConnection(@"Server=localhost;Port=5432;Database=new_database;User Id=aidan; Password=user;"))
+            using (IDbConnection conn = new NpgsqlConnection(ConfigurationManager
+                .ConnectionStrings["accessConnectionString"]
+                .ConnectionString))
             {
                 conn.Open();
                 foreach (var item in data)
-                {
                     try
                     {
-                        conn.Execute("insert into mongoose.pack (name) values (@PACK)", new { item.PACK });
+                        conn.Execute("insert into mongoose.pack (name) values (@PACK)", new {item.PACK});
                     }
                     catch (Exception ex)
                     {
                         logger.Error(ex, "insert error " + ex.Message);
                     }
-                   
-                    
-                }
 
-                conn.Close();              
-
+                conn.Close();
             }
         }
 
         public static IEnumerable<NewLifeHistory> GetLifeHistoryDataFromAccess()
         {
-            using (OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\DropBox\Mongoose\NON_replicated Jan 2017.accdb;Persist Security Info=True"))
+            using (var conn = new OleDbConnection(ConfigurationManager
+                .ConnectionStrings["accessConnectionString"]
+                .ConnectionString))
             {
                 conn.Open();
-                var results = conn.Query<NewLifeHistory>(@"SELECT [PACK], [DATE], [INDIV], [CODE], [LITTER]  FROM [NEW LIFE HISTORY] WHERE [CODE] = ""FPREG"" ");
+                var results = conn.Query<NewLifeHistory>(
+                    @"SELECT [PACK], [DATE], [INDIV], [CODE], [LITTER]  FROM [NEW LIFE HISTORY] WHERE [CODE] = ""FPREG"" ");
                 conn.Close();
 
                 return results;
-
             }
         }
     }
 
- class NewLifeHistory
+    internal class NewLifeHistory
     {
-        public string PACK;
-        public string INDIV;
-        public DateTime DATE;
         public string CODE;
+        public DateTime DATE;
+        public string INDIV;
         public string LITTER;
-
-
+        public string PACK;
     }
 }
 

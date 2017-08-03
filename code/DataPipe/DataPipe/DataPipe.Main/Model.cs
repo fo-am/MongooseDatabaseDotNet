77680c41bb2585cd6daa349e8a8dc81e3f2f2366
Aidan Weatherill <aidan@fo.am>
2017-08-03 20:54:37 +0100

    Add data pipe application

---
+++ code/DataPipe/DataPipe/DataPipe.Main/Model.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Data.SQLite;
+namespace DataPipe.Main
+{
+    public class Model
+{
+    public int Id { get; set; }
+    public string data { get; set; }
+
+
+    public static string DbFile
+    {
+        get { return "~\\..\\..\\..\\..\\..\\TestDatabase.db"; }
+    }
+
+    public static SQLiteConnection SimpleDbConnection()
+    {
+        return new SQLiteConnection("Data Source=" + DbFile);
+
+    }
+    }
+}
---
+++ code/DataPipe/DataPipe/DataPipe.Main/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+namespace DataPipe.Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine(GetCustomer().data);
+            Console.ReadLine();
+        }
+        public static Model GetCustomer()
+        {
+         
+            if (!File.Exists(Path.GetFullPath(Model.DbFile))) return null;
+
+            using (var cnn = Model.SimpleDbConnection())
+            {
+                cnn.Open();
+                Model result = cnn.Query<Model>(
+                    @"SELECT id, data FROM test").FirstOrDefault();
+                return result;
+            }
+        }
+    }
+}

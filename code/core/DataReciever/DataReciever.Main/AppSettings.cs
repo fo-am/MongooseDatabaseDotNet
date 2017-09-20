79ed8a53f924a485f99e407bf85ef97678f9dde1
Aidan Weatherill <aidan@fo.am>
2017-09-20 16:15:06 +0100

    remove redundent code

--- code/core/DataReciever/DataReciever.Main/AppSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DataReciever.Main { 
-    public class AppSettings
-    {
-        public string RabbitHostName { get; set; }
-        public string RabbitUsername { get; set; }
-        public string RabbitPassword { get; set; }
-        public string SqliteLocation { get; set; }
-    }
-}

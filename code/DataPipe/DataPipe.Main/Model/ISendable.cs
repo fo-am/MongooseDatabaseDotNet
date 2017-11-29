80fd1967de070741e2c5fb1cd8bb6dda82470fe8
Aidan Weatherill <aidan@fo.am>
2017-11-29 22:31:40 +0000

    break model into individual files.

---
+++ code/DataPipe/DataPipe.Main/Model/ISendable.cs
@@ -0,0 +1,10 @@
+namespace DataPipe.Main.Model
+{
+    public interface ISendable
+    {
+        int sent { get; set; }
+        string UniqueId { get; set; }
+        int entity_id { get; set; }
+        string entity_type { get; set; }
+    }
+}
---
+++ code/DataPipe/DataPipe.Main/Model/IndividualCreated.cs
@@ -0,0 +1,23 @@
+using System;
+
+using DataPipe.Main.Model;
+
+namespace DataPipe.Main
+{
+    public class IndividualCreated : ISendable
+    {
+        public string UniqueId { get; set; }
+        public int entity_id { get; set; }
+        public string entity_type { get; set; }
+
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string LitterCode { get; set; }
+        public string ChipCode { get; set; }
+        public double? CollerWeight { get; set; }
+        public int sent { get; set; }
+        public string PackCode { get; set; }
+        public string PackUniqueId { get; set; }
+    }
+}

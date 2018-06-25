5b3953da229b984271be112e736459db7277008d
Aidan Weatherill <aidan@fo.am>
2018-06-25 14:44:02 +0100

    add test proj update to net 2.1

---
+++ code/DataReceiver/DataReceiver.Test/TestAThing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DataReceiver.Test
+{
+    [TestFixture]
+    public class TestAThing
+    {
+        [Test]
+        public void ThingDoesAThing()
+        {
+            Assert.That("A", Is.SameAs("A"));
+        }
+        [Test]
+        public void ThingDdoesAThing()
+        {
+            Assert.That("b", Is.SameAs("A"));
+        }
+    }
+}
